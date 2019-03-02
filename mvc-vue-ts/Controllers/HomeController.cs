1af34cd307fbab1d00eb8c7733e5900ffcff9c03
Adrian Wąż <226042@student.pwr.edu.pl>
2019-03-02 13:08:36 +0100

    Added Controller Home, View Index

---
+++ mvc-vue-ts/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace mvc_vue_ts.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
